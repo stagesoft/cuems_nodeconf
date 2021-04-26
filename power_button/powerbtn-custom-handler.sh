1731692a696ced01e8e5823bbc3d7d3c181c4793
Ion Reguera <ion@stagelab.net>
2021-04-26 12:16:16 +0200

    update scripts to use cuems.service & cuems.service.firstrun

--- power_button/powerbtn-custom-handler.sh
+++ power_button/powerbtn-custom-handler.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 SHOW_LOCK_FILE=/etc/cuems/show.lock
-AUTOCONF_LOCK_FILE=/etc/cuems/first_time.lock
+SERVICE_FILE=/etc/avahi/services/cuems.service
+FIRST_RUN_SERVICE_FILE=/usr/share/cuems/cuems.service.firstrun
 
 LOGGER_BINARY=/usr/bin/logger
 LOGGER_OPTIONS="-p local0.info -t CUEMS_POWER -i"
@@ -8,7 +9,7 @@ LOGGER_COMMAND="$LOGGER_BINARY $LOGGER_OPTIONS"
 
 
 if test -f "$SHOW_LOCK_FILE"; then
-        $LOGGER_COMMAND "$SHOW_LOCK_FILE exists, exiting without doing anything"
+        $LOGGER_COMMAND "$SHOW_LOCK_FILE exists SHOW RUNNING, exiting without doing anything"
         exit 1
 fi
 
@@ -25,7 +26,8 @@ else
         $LOGGER_COMMAND "process is running, activate reset"
         /usr/sbin/shutdown -c
         /usr/bin/wall Double power button press, restarting in auto-conf mode
-        /usr/bin/touch $AUTOCONF_LOCK_FILE
+        /usr/bin/rm $SERVICE_FILE
+        cp $FIRST_RUN_SERVICE_FILE $SERVICE_FILE
         sleep 1
         /usr/sbin/shutdown -r now "Restarting in autoconf mode"
         $LOGGER_COMMAND "Restarting in autoconf mode"
