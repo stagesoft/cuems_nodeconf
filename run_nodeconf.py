c9ee449cbf7fbd145204ffea15597512415668a5
Ion Reguera <ion@stagelab.coop>
2025-07-24 21:14:55 +0200

    use daemon from cuemutils

---
+++ run_nodeconf.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from cuemsutils.daemon import run_daemon
+from cuemsutils.log import Logger
+from CuemsNodeConf import CuemsNodeConf
+
+
+
+def main():
+    # Create and run engine
+    nodeconf = CuemsNodeConf()
+    Logger.debug('CuemsNodeConf instance created, going start to daemon')
+    run_daemon(nodeconf, 'nodeconf')
+
+if __name__ == '__main__':
+    main()
