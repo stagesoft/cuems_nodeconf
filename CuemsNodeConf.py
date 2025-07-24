9690c02e58b4e067bed2bfbc847669288b5ef2d6
Ion Reguera <ion@stagelab.coop>
2025-07-24 21:03:54 +0200

    use functions transferred to cuemsutils

--- CuemsNodeConf.py
+++ CuemsNodeConf.py
@@ -10,13 +10,14 @@ import shutil
 
 from zeroconf import IPVersion, ServiceInfo, ServiceListener, ServiceBrowser, Zeroconf, ZeroconfServiceTypes
 
-import logging
 
 
 from CuemsAvahiListener import CuemsAvahiListener
 from CuemsNode import CuemsNode, CuemsNodeDict
 
 from cuemsutils.xml.XmlReaderWriter import XmlReader, XmlWriter
+from cuemsutils.timeoutloop import Timeoutloop
+from cuemsutils.log import Logger, logged
 
 CUEMS_CONF_PATH = '/etc/cuems/'
 MAP_SCHEMA_FILE = 'network_map.xsd'
@@ -30,7 +31,7 @@ MASTER_ALIAS='controller.local'
 CONTROLLER_ALIAS='formitgo.local'
 
 '''
-logging.basicConfig(level=logging.DEBUG,
+Logger.basicConfig(level=Logger.DEBUG,
                     format='%(name)s: %(message)s',
                     )
 '''
@@ -41,14 +42,13 @@ class CuemsNodeConf():
 
     def __init__(self):
 
-        self.logger = logging.getLogger('Cuems-NodeConf')
 
         # Conf load manager
         # disable temporally until we got nodeconf again
         # try:
         #     self.cm = ConfigManager(path=CUEMS_CONF_PATH, nodeconf=True)
         # except FileNotFoundError:
-        #     self.logger.critical(
+        #     Logger.critical(
         #         'Node config file could not be found. Exiting !!!!!')
           
         #     exit(-1)
@@ -70,10 +70,10 @@ class CuemsNodeConf():
 
         # Check for first run flag in service file
         if self.node.node_type == CuemsNode.NodeType.firstrun:
-            self.logger.debug("First time conf file detected, triying to autoconfigure node")
+            Logger.debug("First time conf file detected, triying to autoconfigure node")
             self.set_node_type()
         else:
-            self.logger.debug(f"Allready configured as {self.node.node_type.name}")
+            Logger.debug(f"Allready configured as {self.node.node_type.name}")
         
         # If I am master finally wait a bit for slaves to appear on the net
         if self.node.node_type == CuemsNode.NodeType.master:
@@ -89,7 +89,7 @@ class CuemsNodeConf():
                 self.publish_controller_alias()
 
         if self.listener.nodes.firstruns:
-            self.logger.debug('Waiting for some other "first-run" nodes')
+            Logger.debug('Waiting for some other "first-run" nodes')
         while self.listener.nodes.firstruns:
             time.sleep(0.5)
 
@@ -98,7 +98,7 @@ class CuemsNodeConf():
         try:
             self.write_network_map()
         except Exception as e:
-            self.logger.exception(e)
+            Logger.exception(e)
 
         self.update_master_lock_file(os.path.join( CUEMS_CONF_PATH, CUEMS_MASTER_LOCK_FILE))
         # Check if I am the master node
@@ -109,7 +109,7 @@ class CuemsNodeConf():
         # elif self.node.node_type == CuemsNode.NodeType.slave:
         #     sys.exit(101)
 
-        self.logger.debug('Startup complete, notifying systemd')
+        Logger.debug('Startup complete, notifying systemd')
         systemd.daemon.notify('READY=1')
 
          #TODO: add timeout for the waiting loops
@@ -120,26 +120,26 @@ class CuemsNodeConf():
             try:
                 self.ip = netifaces.ifaddresses('bridge0:avahi')[netifaces.AF_INET][0]['addr']
                 self_controller_ip = None
-                logging.debug(f"Found bridge0:avahi interface, IP: {self.ip}")
+                Logger.debug(f"Found bridge0:avahi interface, IP: {self.ip}")
                 return 
             except (ValueError, KeyError):
-                logging.debug("bridge0:avahi interface not found, triying next ones")
+                Logger.debug("bridge0:avahi interface not found, triying next ones")
                 try:
                     self.ip = netifaces.ifaddresses('ethernet1:avahi')[netifaces.AF_INET][0]['addr']
-                    logging.debug(f"Found ethernet1:avahi interface, IP: {self.ip}")
+                    Logger.debug(f"Found ethernet1:avahi interface, IP: {self.ip}")
                 except (ValueError, KeyError):
-                    logging.debug("Waiting for ethernet1:avahi interface to appear")
+                    Logger.debug("Waiting for ethernet1:avahi interface to appear")
     
                 try:
                     self.controller_ip = netifaces.ifaddresses('bond0')[netifaces.AF_INET][0]['addr']
                     if self.ip != None:
-                        logging.debug(f"Found bond0 interface, CONTROLLER IP: {self.controller_ip}")
+                        Logger.debug(f"Found bond0 interface, CONTROLLER IP: {self.controller_ip}")
                         return
                     else:
-                        logging.debug(f"Found bond0 interface, but we are mising ethernet1:avahi interface, continuing")
+                        Logger.debug(f"Found bond0 interface, but we are mising ethernet1:avahi interface, continuing")
                 except (ValueError, KeyError):
-                    logging.debug("Waiting for bond0 interface to appear")
             time.sleep(1)
+                    Logger.debug("Waiting for bond0 interface to appear")
 
     def start_avahi_listener(self):
         # self.listener = CuemsAvahiListener(callback=self.callback)
@@ -150,7 +150,7 @@ class CuemsNodeConf():
 
     def set_node_type(self):
         if not self.listener.nodes.masters:
-            self.logger.debug('No master node on the network, I become MASTER!')
+            Logger.debug('No master node on the network, I become MASTER!')
             self.node.node_type = CuemsNode.NodeType.master
 
             # Copy master node service template
@@ -161,7 +161,7 @@ class CuemsNodeConf():
             self.change_network_to_master()
             self.get_ips()
         else:
-            self.logger.debug('Master present on the in network WE STAY SLAVE')
+            Logger.debug('Master present on the in network WE STAY SLAVE')
             self.node.node_type = CuemsNode.NodeType.slave
 
             # Copy slave node service template
@@ -175,7 +175,7 @@ class CuemsNodeConf():
 
         writer = XmlWriter(schema_name = self.xsd_path, xmlfile = self.map_path, xml_root_tag='CuemsNetworkMap')
         writer.write_from_object(map)
-        self.logger.debug("Network map written to XML")
+        Logger.debug("Network map written to XML")
 
 
     def read_network_map(self):
@@ -198,22 +198,22 @@ class CuemsNodeConf():
             pass
 
     def callback(self, caller_node=None, action=CuemsAvahiListener.Action.ADD):
-        self.logger.debug(f" {action} callback!!!, Node: {caller_node} ")
+        Logger.debug(f" {action} callback!!!, Node: {caller_node} ")
 
         self.check_nodes()
 
     def check_nodes(self):
-        # self.logger.debug(self.listener.nodes)
+        # Logger.debug(self.listener.nodes)
         if self.listener.nodes.masters:
-            self.logger.debug(f"Master node(s):\n{self.listener.nodes.masters}")
+            Logger.debug(f"Master node(s):\n{self.listener.nodes.masters}")
         else:
-            self.logger.debug(f"We have no MASTER!! yet? waiting for it")
+            Logger.debug(f"We have no MASTER!! yet? waiting for it")
         if self.listener.nodes.slaves:
-            self.logger.debug(
+            Logger.debug(
                 f"We have {len(self.listener.nodes.slaves)} slaves")
-            self.logger.debug(f"Slave node(s):\n{self.listener.nodes.slaves}")
+            Logger.debug(f"Slave node(s):\n{self.listener.nodes.slaves}")
         else:
-            self.logger.debug("we have no slaves")
+            Logger.debug("we have no slaves")
 
     def check_first_run(self):
         for node in self.listener.nodes.firstruns:
@@ -241,16 +241,16 @@ class CuemsNodeConf():
     def publish_master_alias(self):
         try:
             subprocess.Popen(["avahi-publish", "-aR", MASTER_ALIAS, self.ip], close_fds=True)
-            self.logger.debug(f"Publishing {MASTER_ALIAS} alias in  {self.ip}")
+            Logger.debug(f"Publishing {MASTER_ALIAS} alias in  {self.ip}")
         except Exception as e:
-            self.logger.debug(f"error publishing alias, {type(e)}. {e}")
+            Logger.debug(f"error publishing alias, {type(e)}. {e}")
 
     def publish_controller_alias(self):
         try:
             subprocess.Popen(["avahi-publish", "-aR", CONTROLLER_ALIAS, self.controller_ip], close_fds=True)
-            self.logger.debug(f"Publishing {CONTROLLER_ALIAS} alias in  {self.controller_ip}")
+            Logger.debug(f"Publishing {CONTROLLER_ALIAS} alias in  {self.controller_ip}")
         except Exception as e:
-            self.logger.debug(f"error publishing alias, {type(e)}. {e}")
+            Logger.debug(f"error publishing alias, {type(e)}. {e}")
 
     def update_master_lock_file(self, path):
         if self.node.node_type == CuemsNode.NodeType.master:
@@ -258,16 +258,16 @@ class CuemsNodeConf():
                 try:
                     with open(path, 'a') as results_file:
                         results_file.write('\n')
-                    self.logger.debug("Created new master file")
+                    Logger.debug("Created new master file")
                 except:
-                    self.logger.warning("could not write master lock file")
+                    Logger.warning("could not write master lock file")
         else:
             if os.path.isfile(path):
                 try:
                     os.remove(path)
-                    self.logger.debug("Removed master file")
+                    Logger.debug("Removed master file")
                 except OSError:
-                    self.logger.warning("could not delete master lock file")
+                    Logger.warning("could not delete master lock file")
     
     def change_network_to_master(self):
         try:
@@ -276,20 +276,20 @@ class CuemsNodeConf():
             manager = dbus.Interface(systemd1, 'org.freedesktop.systemd1.Manager')
             
             job = manager.StopUnit('networking.service', 'fail')
-            self.logger.debug("Stopping networking service")
+            Logger.debug("Stopping networking service")
             time.sleep(10)
             self.change_network_settings_to_master()
             job = manager.StartUnit('networking.service', 'fail')
-            self.logger.debug("Starting networking service")
+            Logger.debug("Starting networking service")
             time.sleep(10)
-            self.logger.debug("Networking service restarted successfully")
+            Logger.debug("Networking service restarted successfully")
             job = manager.StartUnit('avahi-daemon.service', 'fail')
             time.sleep(10)
-            self.logger.debug("Avahi daemon restarted successfully, continuing")
+            Logger.debug("Avahi daemon restarted successfully, continuing")
             return True
 
         except Exception as e:
-            self.logger.error(f"Error restarting networking service: {e}")
+            Logger.error(f"Error restarting networking service: {e}")
             return False
         
     def change_network_settings_to_master(self):
@@ -300,11 +300,10 @@ class CuemsNodeConf():
             target = '/etc/network/interfaces'
             shutil.copy2(source, target)
         except Exception as e:
-            self.logger.error(f"Error copying interfaces file: {e}")
         
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG,
                         format='%(name)s: %(message)s',
                         )
-    CuemsNodeConf()
+    CuemsNodeConf()            Logger.error(f"Error copying interfaces file: {e}")            Logger.error(f"Error copying interfaces file: {e}")
